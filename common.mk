15951bac18b3a4d3f906e1016a7a301eed5b32e9
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2022-06-11 02:02:20 +0000

    sm8350-common: Enable freeform windows
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I90d117dfbcc3b0cc759d7bdc193d307ba9022d03
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    (cherry picked from commit 2e455b66dde97448192d56c0f556bfbab59e2bbf)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- common.mk
+++ common.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
