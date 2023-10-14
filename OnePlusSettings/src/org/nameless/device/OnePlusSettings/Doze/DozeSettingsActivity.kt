chandu078/device_oneplus_sm8350-common

sm8350-common: Implement OnePlus Settings support
Taken from https://github.com/AOSiP-Devices/device_oneplus_sm8150-common/commits/eleven/DeviceSettings.
[cjh1249131356]:
- Fully refractor, cleanup duplicated codes everywhere.
- Write/Pick in more features.

Includes:
- Mute media volume on silent mode toggle
- HBM & Auto HBM + QS tile
- Ambient display + QS tile (merged oplusdoze into OnePlusSettings)
- FPS Info + QS tile
- Hyper Touch + QS tile
- Tp Edge Limit toggle (Unlock edge touch) + QS tile
- Vibrator strength preference
- CustomSeekbarPreference & CustomSwitchPreference (Include haptic feedback)

Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

OnePlusSettings/src/org/nameless/device/OnePlusSettings/Doze/DozeSettingsActivity.kt
@@ -1,0 +1,24 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.device.OnePlusSettings.Doze
+
+import android.os.Bundle
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
+import com.android.settingslib.collapsingtoolbar.R
+
+class DozeSettingsActivity : CollapsingToolbarBaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fragmentManager
+            .beginTransaction()
+            .replace(R.id.content_frame, DozeSettingsFragment(), TAG)
+            .commit()
+    }
+
+    companion object {
+        private const val TAG = "DozeSettingsActivity"
+    }
+}

OnePlusSettings/src/org/nameless/device/OnePlusSettings/MainSettingsActivity.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2017 The OmniROM Project
+ * Copyright (C) 2022 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.device.OnePlusSettings;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class MainSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private MainSettings mMainSettingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.content_frame);
+        if (fragment == null) {
+            mMainSettingsFragment = new MainSettings();
+            getFragmentManager().beginTransaction()
+                .add(R.id.content_frame, mMainSettingsFragment)
+                .commit();
+        } else {
+            mMainSettingsFragment = (MainSettings) fragment;
+        }
+    }
+}

delano-git/android_device_oneplus_sm8350-common

OnePlusSettings/src/org/nameless/device/OnePlusSettings/Doze/DozeSettingsActivity.kt
@@ -1,0 +1,24 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.device.OnePlusSettings.Doze
+
+import android.os.Bundle
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
+import com.android.settingslib.widget.R
+
+class DozeSettingsActivity : CollapsingToolbarBaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fragmentManager
+            .beginTransaction()
+            .replace(R.id.content_frame, DozeSettingsFragment(), TAG)
+            .commit()
+    }
+
+    companion object {
+        private const val TAG = "DozeSettingsActivity"
+    }
+}

OnePlusSettings/src/org/nameless/device/OnePlusSettings/MainSettingsActivity.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2017 The OmniROM Project
+ * Copyright (C) 2022 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.device.OnePlusSettings;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class MainSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private MainSettings mMainSettingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.content_frame);
+        if (fragment == null) {
+            mMainSettingsFragment = new MainSettings();
+            getFragmentManager().beginTransaction()
+                .add(R.id.content_frame, mMainSettingsFragment)
+                .commit();
+        } else {
+            mMainSettingsFragment = (MainSettings) fragment;
+        }
+    }
+}