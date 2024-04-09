19e2a19845a829c0020b06b2056d1176ac21df24
Sugakesshaa <sugakesshaa@pixelos.net>
2024-04-09 13:44:28 +0800

    sm8350-common: dolby: Forcefully disable volume-leveler
    Change-Id: I2f5773bdf4700ceb42c7afde842ef71431989931

--- extract-files.sh
+++ extract-files.sh
@@ -68,6 +68,9 @@ function blob_fixup() {
             sed -i "/SystemCamera = / s/1;/0;/g" "${2}"
             sed -i "/SystemCamera = / s/0;$/1;/" "${2}"
             ;;
+        odm/etc/dolby/multimedia_dolby_dax_default.xml)
+            sed -i "/volume-leveler-enable/ s/true/false/g" "${2}"
+            ;;
         product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml)
             sed -i "s/\/my_product/\/product/" "${2}"
             ;;
