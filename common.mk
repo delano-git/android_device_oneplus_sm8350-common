1801707d440a884555189d9ba7348ab9142e40b3
Lup Gabriel <gwolf2u.website@gmail.com>
2025-06-30 19:07:17 +0300

    sm8350-common: enable `OVERRIDE_ENABLE_UFFD_GC`

--- common.mk
+++ common.mk
@@ -191,6 +191,7 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_oplus)
 
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
+OVERRIDE_ENABLE_UFFD_GC := true
 
 # Media
 PRODUCT_COPY_FILES += \
